e1e71649143a9c5a73a2232f8dd0530c27c2000c
Arushi A <arushiagarwal14@gmail.com>
2021-12-09 11:58:04 -0500

    first commit

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 40,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 100,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6
+    },
+}, {timestamps: true})
+
+module.exports = mongoose.model("User", UserSchema);
